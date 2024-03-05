35a88c65194ac4724520aba9a6e5fe12a2a093aa
Doink <kokjaco2@gmail.com>
2024-03-05 23:20:04 +0200

    Firefox and git ignore

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -78,15 +78,6 @@
     distrobox
     nodePackages_latest.pnpm
   ];
-  # home.file = {
-  #   ".mozilla/firefox/doink/chrome" = {
-  #     source = "github:rafaelmardojai/firefox-gnome-theme";
-  #     recursive = true;
-  #   };
-  # };
-  # home.file.".mozilla/firefox/doink/chrome".source = builtins.fetchGit {
-  #   url = "github:rafaelmardojai/firefox-gnome-theme";
-  # };
   home.file.".mozilla/firefox/doink/chrome".source = inputs.firefox-gnome-theme;
 
   programs = {
@@ -109,28 +100,29 @@
     jq.enable = true;
     firefox = {
       enable = true;
-      # profiles.doink = {
-      #   settings = {
-      #     "extensions.pocket.enabled" = false;
-      #     "gfx.webrender.all" = true;
-      #     "media.ffmpeg.vaapi.enabled" = true;
-      #     "media.ffvpx.enabled" = false;
-      #     "media.navigator.mediadataencoder_vpx_enabled" = true;
-      #     "media.rdd-ffmpeg.enabled" = true;
-      #     "ui.key.menuAccessKey" = 0; # Hide access key underlining
-      #     "browser.uidensity" = 0;
-      #     "gnomeTheme.activeTabContrast" = true;
-      #     "gnomeTheme.bookmarksToolbarUnderTabs" = true;
-      #     "gnomeTheme.hideSingleTab" = false;
-      #     "gnomeTheme.hideWebrtcIndicator" = true;
-      #     "gnomeTheme.spinner" = true;
-      #     "gnomeTheme.normalWidthTabs" = true;
-      #     "gnomeTheme.tabsAsHeaderbar" = false;
-      #     "layers.acceleration.force-enabled" = true;
-      #     "svg.context-properties.content.enabled" = true;
-      #     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      #     "widget.gtk.overlay-scrollbars.enabled" = true;
-      #   };
+      profiles.test = {
+        settings = {
+          "extensions.pocket.enabled" = false;
+          "gfx.webrender.all" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "media.ffvpx.enabled" = false;
+          "media.navigator.mediadataencoder_vpx_enabled" = true;
+          "media.rdd-ffmpeg.enabled" = true;
+          "ui.key.menuAccessKey" = 0; # Hide access key underlining
+          "browser.uidensity" = 0;
+          "gnomeTheme.activeTabContrast" = true;
+          "gnomeTheme.bookmarksToolbarUnderTabs" = true;
+          "gnomeTheme.hideSingleTab" = false;
+          "gnomeTheme.hideWebrtcIndicator" = true;
+          "gnomeTheme.spinner" = true;
+          "gnomeTheme.normalWidthTabs" = true;
+          "gnomeTheme.tabsAsHeaderbar" = false;
+          "layers.acceleration.force-enabled" = true;
+          "svg.context-properties.content.enabled" = true;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "widget.gtk.overlay-scrollbars.enabled" = true;
+        };
+      };
     };
     starship = {
       enable = true;
