5cc52b78a601f26af7b005c4c770398e0916bf1f
Jaco Kok <kokjaco2@gmail.com>
2024-04-01 17:19:19 +0200

    Printer fix

--- modules/shared.nix
+++ modules/shared.nix
@@ -38,6 +38,22 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+  services.printing = {
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
