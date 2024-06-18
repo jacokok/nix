5f081e5fc85a6c76330468b285d6386fd1550986
Jaco Kok <kokjaco2@gmail.com>
2024-06-19 01:06:46 +0200

    Latest

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-#nix flake update
+# nix flake update
+# sudo nixos-rebuild boot
+# sudo poweroff --reboot
+# sudo nix-collect-garbage -d
+# sudo /run/current-system/bin/switch-to-configuration boot
 # sudo nixos-rebuild switch --flake .#doink-laptop
 # sudo nixos-rebuild switch --flake .#doink
 # sudo nixos-rebuild switch --flake .
