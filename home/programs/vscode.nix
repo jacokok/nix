409e2fe542efe7a02a3a14490690c3ee89e4fd50
Jaco Kok <kokjaco2@gmail.com>
2024-07-01 20:04:40 +0200

    Latest and vscode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -4,6 +4,18 @@
     vscode = {
       enable = true;
       # enableUpdateCheck = false;
+      package = pkgs.vscode.fhs;
+      # package = pkgs.vscode.fhsWithPackages (ps: with ps; [
+      #   zlib
+      #   glibc
+      #   curl
+      #   openssl
+      #   gdb
+      #   (with dotnetCorePackages;
+      #   combinePackages [
+      #     sdk_8_0
+      #   ])
+      # ]);
     };
   };
 }
