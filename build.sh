6ba8fc76ecdf8be143cfc666ba2c2bbb20cd1ccf
Jaco Kok <kokjaco2@gmail.com>
2024-07-09 11:29:03 +0200

    Multiple fixes Commented out blender-hip

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-# nix flake update
+# Update flake
+nix flake update
+
+# Build
+sudo nixos-rebuild build --flake .
+sudo nixos-rebuild build --flake . --option eval-cache false
+sudo nixos-rebuild build --flake . --option show-trace true --option eval-cache false
+
 # sudo nixos-rebuild boot
 # sudo poweroff --reboot
 # sudo nix-collect-garbage -d
@@ -7,6 +14,8 @@
 # sudo nixos-rebuild switch --flake .#doink
 # sudo nixos-rebuild switch --flake .
 # sudo nixos-rebuild build --flake . --option eval-cache false
+
+sudo nixos-rebuild build --flake . --option show-trace true --option eval-cache fals
 
 sudo nixos-rebuild switch --flake . --option eval-cache false
 
