40daf78ce4b71efbb9bd4c6c840d793b5ecc02f2
Jaco Kok <kokjaco2@gmail.com>
2024-07-23 23:55:41 +0200

    Playwright ideas.

--- home/packages/playwright.nix
+++ home/packages/playwright.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }: {
+
   home.packages = with pkgs; [
     playwright-driver.browsers
   ];
 
   home.sessionVariables = {
     PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
     PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
   };
+
+  # environment.variables = { };
 }
