652ade14bd525ef53baa2669a695220dc818511a
Jaco Kok <kokjaco2@gmail.com>
2024-08-30 13:43:34 +0200

    vscode working

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -3,38 +3,20 @@
   programs = {
     vscode = {
       enable = true;
-      # enableUpdateCheck = false;
-      # package = pkgs.vscode.fhs;
-      # package = pkgs.vscode.fhsWithPackages (ps: with ps; [
-      #   glib
-      #   glibc.static
-      #   zlib.static
-      #   icu.dev
-      #   musl
-      #   openssl
-      # ]);
-      # package = pkgs.vscode.fhsWithPackages (ps: with ps; [
-      #   zlib
-      #   glibc
-      #   curl
-      #   openssl
-      #   gdb
-      #   (with dotnetCorePackages;
-      #   combinePackages [
-      #     sdk_8_0
-      #   ])
-      # ]);
+
+      package = pkgs.vscode;
 
       mutableExtensionsDir = true;
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
 
-      extensions = with pkgs.vscode-marketplace; [
-        ms-azuretools.vscode-docker
+      extensions = (with (pkgs.forVSCodeVersion pkgs.vscode.version).vscode-marketplace; [
         ms-dotnettools.vscode-dotnet-runtime
+        # ms-dotnettools.csharp
+        # ms-dotnettools.csdevkit
+        ms-azuretools.vscode-docker
         jacokok.csharp-stretch
         streetsidesoftware.code-spell-checker
-        codeium.codeium
         ms-vscode-remote.remote-containers
         mikestead.dotenv
         dbaeumer.vscode-eslint
@@ -53,33 +35,29 @@
         ms-python.python
         ms-python.debugpy
         redhat.vscode-commons
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
+        # ms-vscode-remote.remote-ssh
+        # ms-vscode-remote.remote-ssh-edit
         ms-vscode.remote-explorer
         svelte.svelte-vscode
         bradlc.vscode-tailwindcss
         tomoki1207.pdf
         redhat.vscode-yaml
-      ]
-      ++
-      (with pkgs.vscode-extensions;
-      [
-        ms-dotnettools.csharp
+        #codeium.codeium
+      ]) ++ (with pkgs.vscode-extensions; [
         ms-dotnettools.csdevkit
+        ms-dotnettools.csharp
       ]);
 
       userSettings = {
-        "editor" = {
-          "smoothScrolling" = true;
-          "acceptSuggestionOnCommitCharacter" = false;
-          "fontFamily" = "'FiraCode Nerd Font', 'Fira Code', Consolas";
-          "fontLigatures" = true;
-          "minimap.maxColumn" = 100;
-          "cursorBlinking" = "smooth";
-          "cursorSmoothCaretAnimation" = "on";
-          "semanticHighlighting.enabled" = true;
-          "formatOnSave" = true;
-        };
+        "editor.smoothScrolling" = true;
+        "editor.acceptSuggestionOnCommitCharacter" = false;
+        "editor.fontFamily" = "'FiraCode Nerd Font', 'Fira Code', Consolas";
+        "editor.fontLigatures" = true;
+        "editor.minimap.maxColumn" = 100;
+        "editor.cursorBlinking" = "smooth";
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.semanticHighlighting.enabled" = true;
+        "editor.formatOnSave" = true;
 
         "workbench.list.smoothScrolling" = true;
         "workbench.iconTheme" = "material-icon-theme";
@@ -90,18 +68,20 @@
           "statusBar.background" = "#323233";
           "statusBarItem.remoteBackground" = "#323233";
         };
+
         "breadcrumbs.enabled" = false;
         "window.titleBarStyle" = "custom";
         "window.zoomLevel" = 2;
         "window.commandCenter" = false;
         "explorer.confirmDelete" = false;
-        "git" = {
-          "confirmSync" = false;
-          "mergeEditor" = true;
-          "autofetch" = true;
-          "openRepositoryInParentFolders" = "never";
-        };
 
+        "git.confirmSync" = false;
+        "git.mergeEditor" = true;
+        "git.autofetch" = true;
+        "git.openRepositoryInParentFolders" = "never";
+
+        "extensions.autoCheckUpdates" = false;
+        "extensions.autoUpdate" = false;
       };
     };
   };
