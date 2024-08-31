a8e23d59553845987b5888e53365fea0155cfb37
Jaco Kok <kokjaco2@gmail.com>
2024-08-31 16:08:45 +0200

    Using only c# from nix the rest from vscode

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -11,139 +11,147 @@ with pkgs;
       package = pkgs.vscode;
 
       mutableExtensionsDir = true;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
+      # enableUpdateCheck = false;
+      # enableExtensionUpdateCheck = false;
 
-      extensions = (with (pkgs.forVSCodeVersion pkgs.vscode.version).vscode-marketplace; [
-        ms-dotnettools.csdevkit
+      extensions = (with pkgs.vscode-marketplace; [
+        plugins.ms-dotnettools.csdevkit
         ms-dotnettools.vscode-dotnet-runtime
-        # ms-dotnettools.csharp
-        # ms-dotnettools.csdevkit
-        ms-azuretools.vscode-docker
-        jacokok.csharp-stretch
-        streetsidesoftware.code-spell-checker
-        ms-vscode-remote.remote-containers
-        mikestead.dotenv
-        dbaeumer.vscode-eslint
-        tamasfe.even-better-toml
-        mhutchie.git-graph
-        donjayamanne.githistory
-        github.vscode-github-actions
-        ms-python.isort
-        yzhang.markdown-all-in-one
-        davidanson.vscode-markdownlint
-        pkief.material-icon-theme
-        jnoortheen.nix-ide
-        csstools.postcss
-        esbenp.prettier-vscode
-        yoavbls.pretty-ts-errors
-        ms-python.python
-        ms-python.debugpy
-        redhat.vscode-commons
-        # ms-vscode-remote.remote-ssh
-        # ms-vscode-remote.remote-ssh-edit
-        ms-vscode.remote-explorer
-        svelte.svelte-vscode
-        bradlc.vscode-tailwindcss
-        tomoki1207.pdf
-        redhat.vscode-yaml
-        #codeium.codeium
       ]) ++ (with pkgs.vscode-extensions; [
         ms-dotnettools.csharp
-      ]) ++ (with pkgs.vscode-marketplace; [
-        plugins.vscodevim.vim
       ]);
 
-      userSettings = {
-        "editor.smoothScrolling" = true;
-        "editor.acceptSuggestionOnCommitCharacter" = false;
-       "editor.fontFamily" = "'FiraCode Nerd Font', 'Fira Code', Consolas";
-        "editor.fontLigatures" = true;
-        "editor.minimap.maxColumn" = 100;
-        "editor.cursorBlinking" = "smooth";
-        "editor.cursorSmoothCaretAnimation" = "on";
-        "editor.semanticHighlighting.enabled" = true;
-        "editor.formatOnSave" = true;
-        "editor.minimap.showSlider" = "always";
-        "editor.linkedEditing" = true;
-        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      # extensions = (with (pkgs.forVSCodeVersion pkgs.vscode.version).vscode-marketplace; [
+      #   # ms-dotnettools.csdevkit
+      #   ms-dotnettools.vscode-dotnet-runtime
+      #   # ms-dotnettools.csharp
+      #   # ms-dotnettools.csdevkit
+      #   ms-azuretools.vscode-docker
+      #   jacokok.csharp-stretch
+      #   streetsidesoftware.code-spell-checker
+      #   ms-vscode-remote.remote-containers
+      #   mikestead.dotenv
+      #   dbaeumer.vscode-eslint
+      #   tamasfe.even-better-toml
+      #   mhutchie.git-graph
+      #   donjayamanne.githistory
+      #   github.vscode-github-actions
+      #   ms-python.isort
+      #   yzhang.markdown-all-in-one
+      #   davidanson.vscode-markdownlint
+      #   pkief.material-icon-theme
+      #   jnoortheen.nix-ide
+      #   csstools.postcss
+      #   esbenp.prettier-vscode
+      #   yoavbls.pretty-ts-errors
+      #   ms-python.python
+      #   ms-python.debugpy
+      #   redhat.vscode-commons
+      #   # ms-vscode-remote.remote-ssh
+      #   # ms-vscode-remote.remote-ssh-edit
+      #   ms-vscode.remote-explorer
+      #   svelte.svelte-vscode
+      #   bradlc.vscode-tailwindcss
+      #   tomoki1207.pdf
+      #   redhat.vscode-yaml
+      #   #codeium.codeium
+      # ]) ++ (with pkgs.vscode-extensions; [
+      #   ms-dotnettools.csharp
+      # ]) ++ (with pkgs.vscode-marketplace; [
+      #   plugins.ms-dotnettools.csdevkit
+      #   plugins.ms-dotnettools.csharp
+      # ]);
 
-        "workbench.list.smoothScrolling" = true;
-        "workbench.iconTheme" = "material-icon-theme";
-        "workbench.startupEditor" = "readme";
-        "workbench.layoutControl.enabled" = false;
-        "workbench.sideBar.location" = "right";
-        "workbench.editor.empty.hint" = "hidden";
-        "workbench.colorCustomizations" = {
-          "statusBar.background" = "#323233";
-          "statusBarItem.remoteBackground" = "#323233";
-        };
+      # userSettings = {
+      #   "editor.smoothScrolling" = true;
+      #   "editor.acceptSuggestionOnCommitCharacter" = false;
+      #   "editor.fontFamily" = "'FiraCode Nerd Font', 'Fira Code', Consolas";
+      #   "editor.fontLigatures" = true;
+      #   "editor.minimap.maxColumn" = 100;
+      #   "editor.cursorBlinking" = "smooth";
+      #   "editor.cursorSmoothCaretAnimation" = "on";
+      #   "editor.semanticHighlighting.enabled" = true;
+      #   "editor.formatOnSave" = true;
+      #   "editor.minimap.showSlider" = "always";
+      #   "editor.linkedEditing" = true;
+      #   "editor.defaultFormatter" = "esbenp.prettier-vscode";
 
-        "breadcrumbs.enabled" = false;
+      #   "workbench.list.smoothScrolling" = true;
+      #   "workbench.iconTheme" = "material-icon-theme";
+      #   "workbench.startupEditor" = "readme";
+      #   "workbench.layoutControl.enabled" = false;
+      #   "workbench.sideBar.location" = "right";
+      #   "workbench.editor.empty.hint" = "hidden";
+      #   "workbench.colorCustomizations" = {
+      #     "statusBar.background" = "#323233";
+      #     "statusBarItem.remoteBackground" = "#323233";
+      #   };
 
-        "window.titleBarStyle" = "custom";
-        "window.zoomLevel" = 2;
-        "window.commandCenter" = false;
-        "window.title" = "\${activeEditorShort}\${separator}\${rootName}\${separator}";
+      #   "breadcrumbs.enabled" = false;
 
-        "explorer.confirmDelete" = false;
+      #   "window.titleBarStyle" = "custom";
+      #   "window.zoomLevel" = 2;
+      #   "window.commandCenter" = false;
+      #   "window.title" = "\${activeEditorShort}\${separator}\${rootName}\${separator}";
 
-        "git.confirmSync" = false;
-        "git.mergeEditor" = true;
-        "git.autofetch" = true;
-        "git.openRepositoryInParentFolders" = "never";
+      #   "explorer.confirmDelete" = false;
 
-        "[csharp]" = {
-          "editor.defaultFormatter" = "ms-dotnettools.csharp";
-          "editor.formatOnSave" = true;
-        };
+      #   "git.confirmSync" = false;
+      #   "git.mergeEditor" = true;
+      #   "git.autofetch" = true;
+      #   "git.openRepositoryInParentFolders" = "never";
 
-        "csharp.semanticHighlighting.enabled" = true;
-        "csharp.suppressHiddenDiagnostics" = false;
-        "csharp.maxProjectFileCountForDiagnosticAnalysis" = 0;
-        "dotnet.completion.showCompletionItemsFromUnimportedNamespaces" = true;
-        "dotnetAcquisitionExtension.enableTelemetry" = false;
+      #   "[csharp]" = {
+      #     "editor.defaultFormatter" = "ms-dotnettools.csharp";
+      #     "editor.formatOnSave" = true;
+      #   };
 
-        "[typescript]" = {
-          "editor.tabSize" = 2;
-        };
-        "typescript.updateImportsOnFileMove.enabled" = "always";
+      #   "csharp.semanticHighlighting.enabled" = true;
+      #   "csharp.suppressHiddenDiagnostics" = false;
+      #   "csharp.maxProjectFileCountForDiagnosticAnalysis" = 0;
+      #   "dotnet.completion.showCompletionItemsFromUnimportedNamespaces" = true;
+      #   "dotnetAcquisitionExtension.enableTelemetry" = false;
 
-        "[svelte]" = {
-          "editor.defaultFormatter" = "svelte.svelte-vscode";
-          "editor.formatOnSave" = true;
-          "editor.tabSize" = 2;
-        };
-        "svelte.enable-ts-plugin" = true;
+      #   "[typescript]" = {
+      #     "editor.tabSize" = 2;
+      #   };
+      #   "typescript.updateImportsOnFileMove.enabled" = "always";
 
-        "[nix]" = {
-          "editor.insertSpaces" = true;
-          "editor.tabSize" = 2;
-          "editor.formatOnSave" = true;
-        };
-        "[javascript]" = {
-          "editor.tabSize" = 2;
-        };
-        "javascript.updateImportsOnFileMove.enabled" = "always";
+      #   "[svelte]" = {
+      #     "editor.defaultFormatter" = "svelte.svelte-vscode";
+      #     "editor.formatOnSave" = true;
+      #     "editor.tabSize" = 2;
+      #   };
+      #   "svelte.enable-ts-plugin" = true;
 
-        "eslint.format.enable" = true;
-        "prettier.singleQuote" = false;
+      #   "[nix]" = {
+      #     "editor.insertSpaces" = true;
+      #     "editor.tabSize" = 2;
+      #     "editor.formatOnSave" = true;
+      #   };
+      #   "[javascript]" = {
+      #     "editor.tabSize" = 2;
+      #   };
+      #   "javascript.updateImportsOnFileMove.enabled" = "always";
 
-        "[jsonc]" = {
-          "editor.defaultFormatter" = "vscode.json-language-features";
-        };
-        "[markdown]" = {
-          "editor.defaultFormatter" = "yzhang.markdown-all-in-one";
-        };
-        "[json]" = {
-          "editor.tabSize" = 2;
-        };
-        "yaml.schemaStore.enable" = true;
+      #   "eslint.format.enable" = true;
+      #   "prettier.singleQuote" = false;
 
-        "diffEditor.ignoreTrimWhitespace" = true;
-        "redhat.telemetry.enabled" = true;
-        "security.workspace.trust.untrustedFiles" = "open";
-      };
+      #   "[jsonc]" = {
+      #     "editor.defaultFormatter" = "vscode.json-language-features";
+      #   };
+      #   "[markdown]" = {
+      #     "editor.defaultFormatter" = "yzhang.markdown-all-in-one";
+      #   };
+      #   "[json]" = {
+      #     "editor.tabSize" = 2;
+      #   };
+      #   "yaml.schemaStore.enable" = true;
+
+      #   "diffEditor.ignoreTrimWhitespace" = true;
+      #   "redhat.telemetry.enabled" = true;
+      #   "security.workspace.trust.untrustedFiles" = "open";
+      # };
     };
   };
 }
--- home/programs/vscode/vscode_plugins.nix
+++ home/programs/vscode/vscode_plugins.nix
@@ -9,21 +9,68 @@ merge
   (merge
     (merge
       (merge
-      {
-        "ms-vscode-remote"."remote-ssh" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "remote-ssh";
-          publisher = "ms-vscode-remote";
-          version = "0.113.2024072315";
-          sha256 = "07v2rxrbd1zk7ps06qcx0gyfd9nw38iadw5jsw5l2mrr0gx9vsdk";
+      { }
+        (lib.attrsets.optionalAttrs (isLinux && (isi686 || isx86_64)) {
+          "ms-dotnettools"."csharp" = vscode-utils.extensionFromVscodeMarketplace {
+            name = "csharp";
+            publisher = "ms-dotnettools";
+            version = "2.45.17";
+            sha256 = "034zn8fyxqwyz58giys4sk44l3lzvwva5m3d01zrg60hmaanwj9a";
+            arch = "linux-x64";
+          };
+          "ms-dotnettools"."csdevkit" = vscode-utils.extensionFromVscodeMarketplace {
+            name = "csdevkit";
+            publisher = "ms-dotnettools";
+            version = "1.10.12";
+            sha256 = "1g4vwmyvabbkmc6b07myq3fpjvwr8q7bxba809jlc7hcfqwyw97a";
+            arch = "linux-x64";
+          };
+        }))
+      (lib.attrsets.optionalAttrs (isLinux && (isAarch32 || isAarch64)) {
+        "ms-dotnettools"."csharp" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "csharp";
+          publisher = "ms-dotnettools";
+          version = "2.45.17";
+          sha256 = "1x98i8h8y0zkyv1w5q5ymrp7hlmabgbypl5w9m79wdlai6y6kxvp";
+          arch = "linux-arm64";
         };
-        "vscodevim"."vim" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vim";
-          publisher = "vscodevim";
-          version = "1.28.0";
-          sha256 = "00q528805b13acm3l0shfpb6kjr09h6k9m9bl432a0hm514k61f8";
+        "ms-dotnettools"."csdevkit" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "csdevkit";
+          publisher = "ms-dotnettools";
+          version = "1.10.12";
+          sha256 = "125v68db33ks6fphqla9k4s8rmsx3pqgva3x0f582a9qq3h6jrd4";
+          arch = "linux-arm64";
         };
-      }
-        (lib.attrsets.optionalAttrs (isLinux && (isi686 || isx86_64)) { }))
-      (lib.attrsets.optionalAttrs (isLinux && (isAarch32 || isAarch64)) { }))
-    (lib.attrsets.optionalAttrs (isDarwin && (isi686 || isx86_64)) { }))
-  (lib.attrsets.optionalAttrs (isDarwin && (isAarch32 || isAarch64)) { })
+      }))
+    (lib.attrsets.optionalAttrs (isDarwin && (isi686 || isx86_64)) {
+      "ms-dotnettools"."csharp" = vscode-utils.extensionFromVscodeMarketplace {
+        name = "csharp";
+        publisher = "ms-dotnettools";
+        version = "2.45.17";
+        sha256 = "0qgrv0j8ngsyzh7q14v6xyl93hv7zky997ps2swnqiqlvny9qq57";
+        arch = "darwin-x64";
+      };
+      "ms-dotnettools"."csdevkit" = vscode-utils.extensionFromVscodeMarketplace {
+        name = "csdevkit";
+        publisher = "ms-dotnettools";
+        version = "1.10.12";
+        sha256 = "0yz5c3b6sx2jy51v9d5qkj631krqa6y6v71qhf7h5kvyl23aqk13";
+        arch = "darwin-x64";
+      };
+    }))
+  (lib.attrsets.optionalAttrs (isDarwin && (isAarch32 || isAarch64)) {
+    "ms-dotnettools"."csharp" = vscode-utils.extensionFromVscodeMarketplace {
+      name = "csharp";
+      publisher = "ms-dotnettools";
+      version = "2.45.17";
+      sha256 = "150wh1078zqxaiy68ivdrq70dvlp3lzj62044cpjvp7gcs171nsi";
+      arch = "darwin-arm64";
+    };
+    "ms-dotnettools"."csdevkit" = vscode-utils.extensionFromVscodeMarketplace {
+      name = "csdevkit";
+      publisher = "ms-dotnettools";
+      version = "1.10.12";
+      sha256 = "0phlp8lcl06i53l3cb1k0wbxfp5l07q6vl61by1pvpl4zy8b8azg";
+      arch = "darwin-arm64";
+    };
+  })
