6b9ccad9a88cc4a5b4d9cb988e61acd3a919babf
Jaco Kok <kokjaco2@gmail.com>
2024-10-19 01:07:23 +0200

    No change just tested overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,22 @@
-{ inputs, ... }: {
+{ ... }:
+{
   additions = final: _prev: import ./pkgs final.pkgs;
 
-  modifications = final: prev:
-    {
-      # example = prev.example.overrideAttrs (oldAttrs: rec {
-      # ...
-      # });
-      # nvim = inputs.nvim.packages.${final.system}.default;
-    };
+  modifications = final: prev: {
+    # example = prev.example.overrideAttrs (oldAttrs: rec {
+    # ...
+    # });
+
+    # Overlay worked but simply changing version is not enough
+    # vscode-extensions = final.lib.recursiveUpdate prev.vscode-extensions {
+    #   ms-dotnettools.csharp = final.vscode-utils.buildVscodeMarketplaceExtension {
+    #     mktplcRef = {
+    #       name = "csharp";
+    #       publisher = "ms-dotnettools";
+    #       version = "2.50.27";
+    #       sha256 = "sha256-TSohDtn+VOhPi5FbA7VbzH8kom+GmdMfq5FoMCXlv5U=";
+    #     };
+    #   };
+    # };
+  };
 }
--- overlays/pkgs/default.nix
+++ overlays/pkgs/default.nix
@@ -2,4 +2,5 @@
 # You can build them using 'nix build .#example'
 pkgs: {
   # example = pkgs.callPackage ./example { };
+  # vscode-extensions.ms-dotnettools.csharp = pkgs.callPackage ./csharp.nix { };
 }
