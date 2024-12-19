85d45430a7c60d824ef319f65da382a2a7ed03d0
Jaco Kok <kokjaco2@gmail.com>
2024-12-19 20:02:30 +0200

    Atuin conf

--- home/programs/atuin.nix
+++ home/programs/atuin.nix
@@ -1,12 +1,33 @@
 {
   programs.atuin = {
     enable = true;
+    # flags = [ "--disable-up-arrow" ];
     settings = {
       auto_sync = true;
-      workspaces = false;
       filter_mode = "host";
+      filter_mode_shell_up_key_binding = "session";
       style = "compact";
       enter_accept = true;
+      keymap_mode = "vim-insert";
+      show_help = false;
+      update_check = false;
+
+      stats = {
+        common_subcommands = [
+          "docker"
+          "git"
+          "go"
+          "nix"
+          "systemctl"
+          "tmux"
+        ];
+        ignored_commands = [
+          "cd"
+          "ls"
+          "vi"
+          "vim"
+        ];
+      };
     };
   };
 }
