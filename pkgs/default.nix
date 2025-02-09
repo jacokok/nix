16c247476696ba9f01ae5dd368d0cda58e6baba5
Jaco Kok <kokjaco2@gmail.com>
2025-02-09 15:29:32 +0200

    Changed override to use package version instead.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,3 @@
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
 pkgs: {
-  # example = pkgs.callPackage ./example { };
-  # adwaita-for-steam = pkgs.callPackage ./adwaita-for-steam { };
-  vscode-extensions.ms-dotnettools.csharp = pkgs.callPackage ./csharp { };
+  # vscode-extensions.ms-dotnettools.csharp = pkgs.callPackage ./csharp { };
 }
