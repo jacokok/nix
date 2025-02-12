493873d863b0b512c1df503deda4628a916e433e
Jaco Kok <kokjaco2@gmail.com>
2025-02-12 13:56:33 +0200

    Laptop fix and path fix

--- flake.nix
+++ flake.nix
@@ -82,7 +82,14 @@
 
       # Available through 'home-manager --flake .#doink@doink-pc'
       homeConfigurations = {
-        "doink@doinkpc" = home-manager.lib.homeManagerConfiguration {
+        "doink@doink-pc" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home-manager/home.nix
+          ];
+        };
+        "doink@doink-laptop" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
           modules = [
