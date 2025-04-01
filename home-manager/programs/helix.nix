39e426c083c11c567232729ef401f1b6f079ede6
Jaco Kok <kokjaco2@gmail.com>
2025-04-01 15:00:27 +0200

    New config location of dotfiles.

--- home-manager/programs/helix.nix
+++ home-manager/programs/helix.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [ helix ];
   # home.packages = with pkgs; [ inputs.helix.packages."${pkgs.system}".helix ];
   home.file = {
-    ".config/helix/config.toml".source = "${inputs.dotfiles}/.helix/config.toml";
+    ".config/helix/config.toml".source = "${inputs.dotfiles}/helix/config.toml";
   };
 }
