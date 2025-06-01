31d86ee5ecddb69585ea013f8f8b1814e078de7f
Jaco Kok <kokjaco2@gmail.com>
2025-06-01 15:39:10 +0200

    Config updates

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,13 +1,11 @@
-{ pkgs, ... }: {
-  # Enable the X11 windowing system.
+{ pkgs, ... }:
+{
   services = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
     xserver = {
       enable = true;
       excludePackages = [ pkgs.xterm ];
-
-      # Enable the GNOME Desktop Environment.
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
     };
     gnome.gnome-keyring.enable = true;
   };
