a4f2671b7346cb0d218fb68c32ddeb639d18733e
Jaco Kok <kokjaco2@gmail.com>
2025-07-07 19:54:09 +0200

    Cache did not do anything

--- nixos/doink-laptop/configuration.nix
+++ nixos/doink-laptop/configuration.nix
@@ -17,9 +17,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Cache v2 remove this if you have issues
-  # nix.binaryCaches = [ "https://aseipp-nix-cache.global.ssl.fastly.net" ];
-  nix.settings.substituters = [ "https://aseipp-nix-cache.global.ssl.fastly.net" ];
-
   networking.hostName = "doink-laptop";
 }
--- nixos/doink-pc/configuration.nix
+++ nixos/doink-pc/configuration.nix
@@ -29,9 +29,5 @@
     };
   };
 
-  # Cache v2 remove this if you have issues
-  nix.settings.substituters = [ "https://aseipp-nix-cache.global.ssl.fastly.net" ];
-  # nix.binaryCaches = [ "https://aseipp-nix-cache.global.ssl.fastly.net" ];
-
   networking.hostName = "doink-pc";
 }
