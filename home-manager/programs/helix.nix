c3535139016ec2e14f748931d6af2b71e6f063e7
Jaco Kok <kokjaco2@gmail.com>
2025-07-28 20:39:57 +0200

    Latest helix

--- home-manager/programs/helix.nix
+++ home-manager/programs/helix.nix
@@ -1,8 +1,52 @@
 { pkgs, inputs, ... }:
 {
-  home.packages = with pkgs; [ helix ];
-  # home.packages = with pkgs; [ inputs.helix.packages."${pkgs.system}".helix ];
-  home.file = {
-    ".config/helix/config.toml".source = "${inputs.dotfiles}/helix/config.toml";
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        scrolloff = 5;
+        bufferline = "multiple";
+      cursor-shape = {
+        insert = "bar";
+      };
+      };
+      keys = {
+        normal = {
+          "A-j" = [
+            "extend_to_line_bounds"              
+            "delete_selection"              
+            "paste_after"
+          ];
+          "A-k" = [
+              "extend_to_line_bounds"
+              "delete_selection"
+              "move_line_up"
+              "paste_before"        
+          ];
+          "A-J" = [
+            "extend_to_line_bounds"              
+            "yank"            
+            "paste_after"
+          ];
+          "A-K" = [
+            "extend_to_line_bounds"
+            "yank"
+            "paste_before"
+          ];
+          "C-g" = [
+            ":new"
+            ":insert-output lazygit"
+            ":buffer-close!"
+            ":redraw"
+          ];
+        };
+      };
+    };
   };
+  # home.packages = with pkgs; [ helix ];
+  # home.file = {
+  #   ".config/helix/config.toml".source = "${inputs.dotfiles}/helix/config.toml";
+  # };
 }
